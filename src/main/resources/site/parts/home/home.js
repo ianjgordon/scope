287b82e75f1004d94d8245927137fe72baa07165
Frank <ferenc.takacs@equionconsulting.com>
2017-04-12 15:48:03 +0100

    Form added + indent

--- src/main/resources/site/parts/home/home.js
+++ src/main/resources/site/parts/home/home.js
@@ -1,22 +1,22 @@
-var portal = require('/lib/xp/portal'); // Import the portal library
+var portal    = require('/lib/xp/portal'); // Import the portal library
 var thymeleaf = require('/lib/xp/thymeleaf'); // Import the Thymeleaf library
 
 // Handle the GET request
-exports.get = function(req) {
+exports.get = function (req) {
 
     // Get the country content as a JSON object
     var content = portal.getContent();
 
     // Prepare the model object with the needed data from the content
     var model = {
-        
+
     };
 
-    // Specify the view file to use
-    var view = resolve('home.html');
+	// Specify the view file to use
+	var view = resolve('home.html');
 
-    // Return the merged view and model in the response object
-    return {
+	// Return the merged view and model in the response object
+	return {
         body: thymeleaf.render(view, model)
-    }
+	}
 };
