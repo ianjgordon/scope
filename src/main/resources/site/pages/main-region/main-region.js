70a225bbe32d7ab76c780a8fd0f1894b277b6071
Frank <ferenc.takacs@equionconsulting.com>
2017-04-12 15:08:58 +0100

    init

---
+++ src/main/resources/site/pages/main-region/main-region.js
@@ -0,0 +1,25 @@
+var portal = require('/lib/xp/portal'); // Import the portal library
+var thymeleaf = require('/lib/xp/thymeleaf'); // Import the Thymeleaf library
+
+// Handle the GET request
+exports.get = function(req) {
+
+    // Get the content that is using the page
+    var content = portal.getContent();
+
+    // Extract the main region which contains component parts
+    var mainRegion = content.page.regions.main;
+
+    // Prepare the model that will be passed to the view
+    var model = {
+        mainRegion: mainRegion
+    };
+
+    // Specify the view file to use
+    var view = resolve('main-region.html');
+    
+    // Return the merged view and model in the response object
+    return {
+        body: thymeleaf.render(view, model)
+    }
+};
