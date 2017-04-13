a987c79ed8f6b6378bdf3d9a6496351cd356b13b
Frank <ferenc.takacs@equionconsulting.com>
2017-04-13 17:39:54 +0100

    Restructuring enonic page to include header and footer by default + working on form template

--- src/main/resources/site/pages/default/default.js
+++ src/main/resources/site/pages/default/default.js
@@ -4,17 +4,20 @@ var thymeleaf = require('/lib/xp/thymeleaf'); // Import the Thymeleaf library
 // Handle the GET request
 exports.get = function(req) {
 
-    // Get the country content as a JSON object
+    // Get the content that is using the page
     var content = portal.getContent();
 
-    // Prepare the model object with the needed data from the content
+    // Extract the main region which contains component parts
+    var mainRegion = content.page.regions.main;
+
+    // Prepare the model that will be passed to the view
     var model = {
-        
+        mainRegion: mainRegion
     };
 
     // Specify the view file to use
-    var view = resolve('header.html');
-
+    var view = resolve('default.html');
+    
     // Return the merged view and model in the response object
     return {
         body: thymeleaf.render(view, model)
